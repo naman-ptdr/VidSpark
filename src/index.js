a4ec2b9fdd3cb259d77f7e24c59fafa48baf81ed
Naman_ptdr <namanpatidar5555@gmail.com>
2025-07-20 16:47:39 +0530

    Connect to mongo Atlas

--- src/index.js
+++ src/index.js
@@ -1,13 +1,38 @@
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants";
+// require('dotenv').config()
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
 
-import express from "express"
 
-( async () => {
-    try {
-        mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-    } catch (error) {
-        console.log("ERROR : ", error);
-        throw error
-    }
-})()
+// As early as possible in your application , iport and configure configDotenv;
+
+
+dotenv.config({
+    path: './env'
+})
+
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
+
+// import express from "express"
+
+// ( async () => {
+//     try {
+//         mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//     } catch (error) {
+//         console.log("ERROR : ", error);
+//         throw error
+//     }
+// })()
